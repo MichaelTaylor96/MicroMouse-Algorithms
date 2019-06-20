7e0da643b4017a895b430f8210e536a864f4ab55
MichaelTaylor96 <michael96taylor@gmail.com>
2019-06-20 16:22:52 -0400

    Tried adding recursive search

--- first_algorithm.py
+++ first_algorithm.py
@@ -5,51 +5,96 @@ import random
 def log(string):
     sys.stderr.write("{}\n".format(string))
 
-def cell_state(X, Y, face, cells):
-    newX = X + face[0]
-    newY = Y + face[1]
-    return cells[newX][newY]
+class Mouse():
+    def __init__(self):
+        self.X = 0
+        self.Y = 0
+        self.pos = (0, 0)
+        self.face = 0
+        self.paths = {}
+        self.path = ["", 0]
+        self.choices = []
+
+    def get_choices(self):
+        options = [3, 0, 1]
+        walls = [API.wallLeft(), API.wallFront(), API.wallRight()]
+        choices = [(opt+self.face)%4 for i, opt in enumerate(options) if not walls[i]]
+        return choices
+
+    def turn_to(self, dirto):
+        if (self.face-1)%4 == int(dirto):
+            API.turnLeft()
+            self.face = (self.face-1)%4
+        else:
+            while self.face != int(dirto):
+                API.turnRight()
+                self.face = (self.face+1)%4
+
+    def update_pos(self):
+        dirs = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+        dis = dirs[self.face]
+        self.X += dis[0]
+        self.Y += dis[1]
+        self.pos = (self.X, self.Y)
+
+    def go_back(self, dirto):
+        self.turn_to(dirto)
+        self.go()
+        self.path = self.paths[self.pos][0]
+
+    def go(self):
+        API.moveForward()
+        moves = 1
+        self.update_pos()
+        choices = self.get_choices()
+        while choices and len(choices) < 2:
+            self.turn_to(choices[0])
+            API.moveForward()
+            moves += 1
+            self.update_pos()
+            choices = self.get_choices()
+        self.path[1] += moves
+        self.choices = choices
+
+    def find_best(self, direction):
+        self.turn_to(direction)
+        if self.pos in self.paths:
+            self.paths[self.pos][1] += str(self.face)
+        self.go()
+        if self.pos not in self.paths:
+            self.paths[self.pos] = [[self.path[0], self.path[1]], "{}".format((self.face+2)%4)]
+        elif self.paths[self.pos][0][1] > self.path[1]:
+            self.paths[self.pos][0] = [self.path[0], self.path[1]]
+        if not self.choices:
+            val = self.path[1]
+            self.go_back((self.face-2)%4)
+            return val
+        options = []
+        for choice in self.choices:
+            options.append(self.paths[self.pos][1] + self.find_best(choice))
+        return max(options)
+
 
 def main():
-    X = 0
-    Y = 0
-    face = 0
-    dirs = [[0, 1], [1, 0], [0, -1], [-1, 0]]
-    cards = ["n", "e", "s", "w"]
-    dead = False
-    cells = [["live" for num in range(API.mazeHeight())] for num in range(API.mazeWidth())]
+    bob = Mouse()
     while True:
-        i = 0
-        walls = [API.wallLeft(), API.wallFront(), API.wallRight()]
-        choices = [boolean for boolean in walls if not boolean]
-        if len(choices) > 1:
-            dead = False
-        if dead:
-            API.setColor(X, Y, "g")
-            cells[X][Y] = "dead"
-        if not API.wallLeft() and cell_state(X, Y, dirs[(face-1)%4], cells) == "live":
-            face = (face-1) % 4
-            API.turnLeft()
-        while API.wallFront() or cell_state(X, Y, dirs[face], cells) == "dead":
-            i += 1
-            if i == 2:
-                dead = True
-                API.setColor(X, Y, "g")
-                cells[X][Y] = "dead"
-            face = (face+1) % 4
-            API.turnRight()
-        X += dirs[face][0]
-        Y += dirs[face][1]
-        string = "{X}, {Y}".format(X=X, Y=Y)
-        log(string)
-        API.moveForward()
-        if API.wallLeft():
-            API.setWall(X, Y, cards[(face-1)%4])
-        if API.wallFront():
-            API.setWall(X, Y, cards[face])
-        if API.wallRight():
-            API.setWall(X, Y, cards[(face+1)%4])
-
+        bob.go()
+        if bob.pos not in bob.paths:
+            bob.paths[bob.pos] = [[bob.path[0], bob.path[1]], "{}".format((bob.face+2)%4)]
+        elif bob.paths[bob.pos][0][1] > bob.path[1]:
+            bob.paths[bob.pos] = [[bob.path[0], bob.path[1]], "{}".format((bob.face+2)%4)]
+        log(bob.paths)
+        if not bob.choices:
+            log(bob.paths[bob.pos][1][0])
+            bob.go_back(bob.paths[bob.pos][1][0])
+            log(bob.path)
+        for choice in bob.choices:
+            if str(choice) not in bob.paths[bob.pos][1]:
+                bob.turn_to(choice)
+                break
+        bob.paths[bob.pos][1] += "{}".format(bob.face)
+        bob.path[0] += "{}".format(bob.face)
+        
 
 if __name__ == "__main__":
     main()
